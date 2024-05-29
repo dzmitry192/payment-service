7b0c9c30e8969edef105fc2829c0dbff6f1f1f33
dzmitry192 <dzmitry.sivachenka@innowise.com>
2024-05-29 09:41:16 +0200

    fix error in rent service fallback

--- src/main/java/com/innowise/sivachenko/feign/fallback/RentServiceFallback.java
+++ src/main/java/com/innowise/sivachenko/feign/fallback/RentServiceFallback.java
@@ -4,6 +4,7 @@ import com.innowise.sivachenko.feign.RentServiceFeignClient;
 import com.innowise.sivachenko.model.exception.ServiceNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -11,9 +12,18 @@ import org.springframework.stereotype.Component;
 public class RentServiceFallback implements FallbackFactory<RentServiceFeignClient> {
     @Override
     public RentServiceFeignClient create(Throwable cause) {
-        return rentId -> {
-            log.info("fallback; reason was: {}, {}", cause.getMessage(), cause);
-            throw new ServiceNotFoundException("Rent service is down. We are working to resolve the issue");
+        return new RentServiceFeignClient() {
+            @Override
+            public ResponseEntity<?> getRent(Long rentId) throws ServiceNotFoundException {
+                log.info("Fallback for getRent; reason was: {}, {}", cause.getMessage(), cause);
+                throw new ServiceNotFoundException("Rent service is down. We are working to resolve the issue");
+            }
+
+            @Override
+            public Boolean isRentActive(Long rentId) throws ServiceNotFoundException {
+                log.info("Fallback for isRentActive; reason was: {}, {}", cause.getMessage(), cause);
+                throw new ServiceNotFoundException("Rent service is down. We are working to resolve the issue");
+            }
         };
     }
 }
